8c2c267c40276bd8a0a3af15eedfaba994358bfe
Denis Turenko <denis.turenko@gmail.com>
2024-09-08 19:11:45 +0300

    done movement

---
+++ src/App.styled.ts
@@ -0,0 +1,30 @@
+import { Game } from "./components/Game";
+import { TouchBar } from "./ui/touch-bar/TouchBar";
+import React, { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
+const { Vector3, MathUtils, ArrowHelper, Ray } = THREE;
+const { degToRad, radToDeg } = MathUtils;
+
+export const App = () => {
+  const [leftControlValue, setLeftControlValue] = useState(0);
+  const [rightControlValue, setRightControlValue] = useState(0);
+
+  return (
+    <>
+      <div id="container">
+        <div id="info"></div>
+        <Canvas camera={{ fov: 45, position: [0, 600, 0], far: 3500 }}>
+          <Game
+            leftControlValue={leftControlValue}
+            rightControlValue={rightControlValue}
+            windAngel={Math.PI - degToRad(180)}
+            windSpeeds={[5, 8]}
+          />
+        </Canvas>
+      </div>
+      <TouchBar isLeft onChange={setLeftControlValue} />
+      <TouchBar isRight onChange={setRightControlValue} />
+    </>
+  );
+};
