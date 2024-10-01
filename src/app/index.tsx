820271840322af31d88d2b94b95045e77264d4f8
Denis Turenko <denis.turenko@gmail.com>
2024-10-02 01:26:14 +0300

    no antd

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,28 +7,10 @@ import './index.css';
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 
-import { ConfigProvider, theme } from 'antd';
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: '#173D71',
-          colorPrimaryBorder: '#173D71',
-          handleActiveColor: 'red',
-        },
-
-        // 1. Use dark algorithm
-        algorithm: theme.darkAlgorithm,
-
-        // 2. Combine dark algorithm and compact algorithm
-        // algorithm: [theme.darkAlgorithm, theme.compactAlgorithm],
-      }}
-    >
-      <MantineProvider defaultColorScheme="dark">
-        <Playground />
-      </MantineProvider>
-    </ConfigProvider>
+    <MantineProvider defaultColorScheme="dark">
+      <Playground />
+    </MantineProvider>
   </React.StrictMode>
 );
